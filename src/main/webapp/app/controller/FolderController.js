c6f6c8915fae890bf5e27e4a4646199f2ae373c0
philippe.demanget <ext.philippe.demanget@pmsipilot.com>
2013-07-25 18:33:33 +0200

    Select Folder, TODO only select

--- src/main/webapp/app/controller/FolderController.js
+++ src/main/webapp/app/controller/FolderController.js
@@ -35,8 +35,14 @@ Ext.define('app.controller.FolderController', {
 		var relativePath="";
 		for(var i in folders){
 		    if(! node.isExpanded()){ node.expand()}
-		    relativePath+=node.getPath();
-		    node.findChild("path",relativePath);
+		    relativePath+="/"+folders[i];
+		    //node.findChild("path",relativePath);
+		    node = node.findChildBy(
+		            function(anode){
+		                console.log(anode.data.path+" "+relativePath);
+		                return anode.data.name===folders[i];
+		    });
+		    
 		}
 		node.set('checked', true); 
 	}
