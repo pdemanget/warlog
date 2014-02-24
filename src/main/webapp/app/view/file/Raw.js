88ffddf857a3b79a539ef28e1347180e62f7085b
PDE3056 <pdemanget@objetdirect.com>
2014-02-24 18:36:47 +0100

    Raw text component

---
+++ src/main/webapp/app/view/file/Raw.js
@@ -0,0 +1,86 @@
+/**
+ * List of line of a file.
+ */
+Ext.define('app.view.file.Raw' ,{
+    extend: 'Ext.panel.Panel',
+    alias : 'widget.fileraw',
+    layout: 'fit',
+    items: [{
+        xtype     : 'textareafield',
+        grow      : true,
+        name      : 'message',
+        anchor    : '100%',
+        flex : 1
+    }],
+    store: 'Lines',
+    dockedItems: [{
+	        xtype: 'pagingtoolbar',
+	        dock: 'bottom',
+	        store: 'Lines',
+	        displayInfo: true
+	    },{
+            xtype: 'toolbar',
+            dock: 'top',
+            items: [
+				{
+					xtype: 'checkbox',
+				    name: 'linenumbers',
+				    fieldLabel: 'line numbers ',
+				    checked: true,
+				    margin:10,
+				    labelWidth: 70
+				},
+                {
+                	xtype: 'textfield',
+                    name: 'pattern',
+        	        fieldLabel: 'Filter ',
+        	        labelWidth: 40,
+        	        width: 300,
+        	        margin: '20px 0 0 0'
+                },
+                '->',
+                {
+                    text: 'display as list',
+                    itemId: '1'
+                }
+            ]
+        }
+        ],
+//--
+//--        
+        
+    columns: [
+         {header: 'line',  dataIndex: 'id',  width:50}
+        ,{header: 'A',  dataIndex: 'col1',  flex: 1, renderer: 'htmlEncode'}
+//        ,{header: 'B', dataIndex: 'col2', flex: 1}
+//        ,{header: 'C', dataIndex: 'col3', flex: 1}
+    ],
+    setRawValue: function(text){
+    	this.down('textareafield').setRawValue(text);
+    },
+    initComponent:function(){
+    	this.callParent();
+    	this.storeImpl=Ext.getStore(this.store);
+    	var area=this.down('textareafield[name=message]');
+    	
+    	
+    	var me=this;
+    	this.down('checkbox').on('change',function(){
+    		me.storeImpl.reload();
+    	});
+    	
+    	this.storeImpl.on('load', function(st,models){
+    		var linenumbers=me.down('checkbox').getValue();
+    		console.log("linenumbers "+linenumbers);
+			var txt='';
+			var lab='';
+			for(var i=0;i<models.length;i++){
+				var model=models[i];
+				if (linenumbers) txt+=model.getId()+': ';
+				txt+=model.get('col1')+'\n';
+			}
+			area.setRawValue(txt);
+		});
+    	
+    }
+});
